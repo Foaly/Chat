f1790a930cfce8c8620911cd560e4bf0315dcf0d
Foaly <foaly.f@web.de>
2014-11-17 13:37:32 +0100

    Ensure that the message struct is 150 byte long and that the compiler doesn't rearrange the memory layout.

--- shared/Message.hpp
+++ shared/Message.hpp
@@ -7,6 +7,9 @@ typedef struct {
     uint32_t message_number;
     char user_name[8];
     char message[138];
-} messagePacket;
+} __attribute__((packed)) messagePacket;
+
+// __attribute__((packed)) ensures, that the compiler adds no padding bytes to the struct.
+// It's guaranteed to be 150 bytes long.
 
 #endif //MESSAGE_INCLUDE
